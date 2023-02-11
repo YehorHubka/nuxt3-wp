317d25680519a700dc7972b07bcd14d3a3fad668
YehorHubka <egubka712248@gmail.com>
2023-02-11 15:48:37 +0200

    adding checking for empty posts

--- pages/categories/[slug].vue
+++ pages/categories/[slug].vue
@@ -3,7 +3,27 @@
     <PageHeading>
       <h1 class="text-4xl font-bold">#{{ categories[0].name }} category</h1>
     </PageHeading>
-    <section class="grid sm:grid-cols-3 gap-5 container pb-10">
+    <section class="container" v-if="posts.length === 0">
+      <div class="text-2xl font-bold mb-5">
+        There are no posts in this category yet.
+      </div>
+      <NuxtLink
+        to="/categories"
+        class="
+          py-2
+          px-4
+          inline-block
+          rounded
+          bg-blue-500
+          text-gray-100
+          hover:bg-blue-600
+          ease
+          duration-300
+        "
+        >Go and check another one</NuxtLink
+      >
+    </section>
+    <section v-else class="grid sm:grid-cols-3 gap-5 container pb-10">
       <BlogCard
         v-for="post in posts"
         :key="post.id"
